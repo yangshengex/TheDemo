4a8ca9cfb99e31f64d10a21cdd60f3332d1b30b2
yangshengex <445800385@qq.com>
2016-09-14 17:40:46 +0800

    modify  some Mybatis error   2016/9/13

--- src/main/java/com/ys/demo/mapper/UserInfodao.java
+++ src/main/java/com/ys/demo/mapper/UserInfodao.java
@@ -1,11 +1,15 @@
 package com.ys.demo.mapper;
 
 import com.ys.demo.po.UserInfo;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by yangshe on 2016/9/12.
  */
 public interface UserInfodao {
     public UserInfo getUser();
+    public List<UserInfo> getUserByNameAndPwd(@Param("name") String username, @Param("pwd")String userpwd);
 }
